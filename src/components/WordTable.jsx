edcccc19b0568254be111145c3c306a8c1c41a86
weeht1996 <72547656+weeht1996@users.noreply.github.com>
2023-04-27 20:02:54 +0800

    Completed Intersect function

--- src/components/WordTable.jsx
+++ src/components/WordTable.jsx
@@ -1,4 +1,4 @@
-import {v4 as uuid} from 'uuid'
+import { v4 as uuid } from 'uuid';
 
 export function WordTable() {
 
@@ -65,27 +65,52 @@ export function WordTable() {
         return 0
     }
 
-    // function intersectWords(startPt, word, increments, intersect_location, intersect_idx) {
-    //     console.log(word + " intersected at " + intersect_location + " at index " + intersect_idx)
-    //     // search for intersected word
-
-    //     for (let i in occupied_spots) {
-    //         let current = occupied_spots[i].word
-    //         if (current.path.some(element => element.toString() == intersect_location.toString())) {
-    //             //intersects with current.word
-    //             console.log(current.word)
-    //             //check if same letters
-
-    //             for (let x = 0; x < current.word.length ; x++) {
-    //                 // console.log(word.charAt(x))
-    //                 if (word.includes(current.word.charAt(x))) {
+    function intersectWords(word, intersect_location) {
+        // search for intersected word
+        searchLoop: for (let i in occupied_spots) {
+            let current = occupied_spots[i].word
+            
+            if (current.path.some(element => element.toString() == intersect_location.toString())) { //finds the word that was intersected
+                
+                if ((word.direction + 4 == current.direction) || (word.direction - 4 == current.direction) || (word.direction == current.direction)) {
+                    return false
+                }
+                
+                for (let x = 0; x < current.word.length ; x++) {
+                    
+                    if (word.word.includes(current.word.charAt(x))) { //finds letters that matches, x is index of letter in intersected word
+                        // console.log(word.word + " intersected at " + intersect_location + ' with starting location of ' + word.spot + ' facing ' + getDirection(word.direction))
+                        //currently avoids duplicate letter overlaps because of indexOf returns 1st found
                         
-    //                 }
-    //             }
-    //         }
-    //     }
+                        let repeated_word_idx = word.word.indexOf(current.word.charAt(x))
+                        let repeated_word_location = [...word.spot]
+                        let increments = getDirection(word.direction)
 
-    // }
+                        repeated_word_location[0] += (increments[0] * repeated_word_idx)
+                        repeated_word_location[1] += (increments[1] * repeated_word_idx)
+                        
+                        let overlap_location = [...current.path[x]]
+                        let shift = [ (overlap_location[0] - repeated_word_location[0]),  (overlap_location[1] - repeated_word_location[1]) ]
+                        let test_location = [(word.spot[0] + shift[0] ), (word.spot[1] + shift[1])]
+                        if (test_location[0] < 0 || test_location[0] > rows || test_location[1] < 0 || test_location[1] > cols) {
+                            console.log('OOB')
+                            return false
+                        }
+                        // console.log(word.word + " shift: " + shift + ' and word spot 5: ' + word.spot + ' and new start point: ' + test_location)
+                        word.spot = test_location
+                        if (isPathClear(word).length > 1) {
+                            console.log('too many collisions')
+                            return false
+                        }
+                        return true
+                        
+                    }
+                }
+            }
+            return false
+        }
+
+    }
 
     function placeWord(word) {
         var increments = getDirection(word.direction)
@@ -110,45 +135,28 @@ export function WordTable() {
         return true
     }
 
-    function checkLocation(word) {
-        let count = 0
-
-        while (count < 8) {
-        //check if available
-            let increments = getDirection(word.direction)
-
-            if (!isInBoundary(word)) {
-                word.direction = changeDirection(word.direction)
-                console.log('boundary fail')
-            } else {
-                for (let x = 0; x < word.word.length; x++) { //under works, for checking if the path is empty
-
-                    console.log('word: ' + word.word + ' at letter: ' + word.word.charAt(x) + ' board spot row: ' + word.spot[0] + ' increment row is ' + increments[0] + ' and x: ' + x)
-
-                    if (board[word.spot[0] + x * increments[0]][word.spot[1] + x * increments[1]] !== null) {
-                        // if (intersect) {intersectWords([row, col], word.word, word.direction, [curr_row, curr_col], x)}
-                        return false
-                    }
-                } //==========================================================================================
-                placeWord(word)
-                return true
+    function isPathClear(word) {
+        let increments = getDirection(word.direction)
+        let collisions = []
+        for (let x = 0; x < word.word.length; x++) { 
+            if (board[word.spot[0] + x * increments[0]][word.spot[1] + x * increments[1]] !== null) {
+                collisions.push( [word.spot[0] + x * increments[0], word.spot[1] + x * increments[1]] )
             }
-            count++
-
         }
-        return false
+        return collisions
     }
 
-
     let words = ['ghost', 'tavern' , 'scary', 'horror', 'thriller', 'tragedy', 'creepy', 'blood', 'nightmare', 'halloween', 'spooky']
-    words.sort((a,b) => b.length - a.length)
-
     var rows = 16
     var cols = 24
-
+    
+    words.sort((a,b) => b.length - a.length)
+    
     var all_spots = getBoard(rows, cols)
     var board = Array.from({ length: rows }, () => Array.from({ length: cols }, () => null));
     var occupied_spots = [] //for sequence of what's already successfully placed [row, col, index of word in words]
+
+    var count = 0
 
     for ( let idx in words) {
         //save current board
@@ -166,8 +174,24 @@ export function WordTable() {
 
         while (!valid && current.length > 0) { //check if location is valid
 
-            // valid = checkLocation(word.spot[0],word.spot[1], word.direction, word.word) //check and places
-            valid = checkLocation(word) //check and places
+            count = 0
+            while (count < 8) {
+                if (isInBoundary(word)) {
+                    let intersected = isPathClear(word)
+                    if (intersected.length == 0) {
+                        placeWord(word)
+                        valid = true 
+                        break
+                    } else if ( window.intersect && intersectWords(word, intersected)) {
+                        placeWord(word)
+                        valid = true 
+                        break
+                    }
+                } 
+                word.direction = changeDirection(word.direction)
+                count++
+            }
+
             //invalid
             current.splice(spot_idx,1)
             spot_idx = getRandomInt(current.length)
@@ -181,7 +205,7 @@ export function WordTable() {
         //if still invalid revert to previous board
     }
 
-    // console.log(occupied_spots)
+    console.log(occupied_spots)
 
     let answer = JSON.parse(JSON.stringify(board))
     for (let j = 0; j < rows; j++) {
